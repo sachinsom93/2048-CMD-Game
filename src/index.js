eb79140cb0cb23bf68fd7f9fe3731461bfbec59c
pawan941394 <pawan941394@gmail.com>
2022-03-23 11:24:13 +0530

    bug: Corrected some spell erros in src/index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const { keypress } = require("./Dependencies/keypress");
 
 /**
  * Project Utilities Function.
-*/
+ */
 const config = require('../config.json');
 const { displayGrid } = require('./Utils/displayGrid');
 const { fillRandomCell } = require('./Utils/fillRandomCell');
 const { startGame } = require('./Utils/startGame');
 const { moveGridToLeft } = require('./Movement/moveToLeft');
-const { moveToRight }= require('./Movement/moveToRight');
+const { moveToRight } = require('./Movement/moveToRight');
 const { moveToDown } = require('./Movement/moveToDown');
 const { moveToUp } = require('./Movement/moveToUp');
 const { gridSize } = require("../config.json");
@@ -20,7 +20,7 @@ const { gridSize } = require("../config.json");
 
 /**
  * Global Vars
-*/
+ */
 const GRIDSIZE = gridSize;
 const WELCOME_MSG = 'Welcome to 2048 CMD Game!';
 
@@ -28,7 +28,7 @@ const WELCOME_MSG = 'Welcome to 2048 CMD Game!';
 
 /**
  * States of the game.
-*/
+ */
 var grid = startGame(GRIDSIZE);
 var score = [0];
 console.log(WELCOME_MSG);
@@ -37,82 +37,82 @@ displayGrid(grid, score);
 
 
 /**
- * Listent for Keypress Event.
-*/
-process.stdin.on("keypress", function (ch, key) {
+ * Listen for Keypress Event.
+ */
+process.stdin.on("keypress", function(ch, key) {
 
-  // Check for key or number chars existance
-  if(!key && !ch) {
-    console.log('Please Use the Arrow Keys.')
-    return;
-  }
+    // Check for key or number chars existence
+    if (!key && !ch) {
+        console.log('Please Use the Arrow Keys.')
+        return;
+    }
 
-  // Extract Key name
-  let keyName = key ? key.name : ch;
+    // Extract Key name
+    let keyName = key ? key.name : ch;
 
-  // Functions for each directions
-  switch (keyName) {
-    case "up":
-    case "3":
-      let movedUp = moveToUp(grid, GRIDSIZE, score);
-      if(movedUp) {
-        var haveEmptyCells = fillRandomCell(grid, GRIDSIZE);
-        if(!haveEmptyCells) {
-          console.log(config.overMsg);
-          process.exit();
-        }
-      }
-      displayGrid(grid, score);
-      break;
+    // Functions for each directions
+    switch (keyName) {
+        case "up":
+        case "3":
+            let movedUp = moveToUp(grid, GRIDSIZE, score);
+            if (movedUp) {
+                var haveEmptyCells = fillRandomCell(grid, GRIDSIZE);
+                if (!haveEmptyCells) {
+                    console.log(config.overMsg);
+                    process.exit();
+                }
+            }
+            displayGrid(grid, score);
+            break;
 
-    case "down":
-    case "4":
-      let movedDown = moveToDown(grid, GRIDSIZE, score);
-      if(movedDown) {
-        var haveEmptyCells = fillRandomCell(grid, GRIDSIZE);
-        if(!haveEmptyCells) {
-          console.log(config.overMsg);
-          process.exit();
-        }
-      }
-      displayGrid(grid, score);
-      break;
+        case "down":
+        case "4":
+            let movedDown = moveToDown(grid, GRIDSIZE, score);
+            if (movedDown) {
+                var haveEmptyCells = fillRandomCell(grid, GRIDSIZE);
+                if (!haveEmptyCells) {
+                    console.log(config.overMsg);
+                    process.exit();
+                }
+            }
+            displayGrid(grid, score);
+            break;
 
-    case "right":
-    case "2":
-      let movedRight = moveToRight(grid, GRIDSIZE, score);
-      if(movedRight) {
-        var haveEmptyCells = fillRandomCell(grid, GRIDSIZE);
-        if(!haveEmptyCells) {
-          console.log(config.overMsg);
-          process.exit();
-        }
-      }
-      displayGrid(grid, score);
-      break;
+        case "right":
+        case "2":
+            let movedRight = moveToRight(grid, GRIDSIZE, score);
+            if (movedRight) {
+                var haveEmptyCells = fillRandomCell(grid, GRIDSIZE);
+                if (!haveEmptyCells) {
+                    console.log(config.overMsg);
+                    process.exit();
+                }
+            }
+            displayGrid(grid, score);
+            break;
 
-    case "left":
-    case "1":
-      let movedLeft = moveGridToLeft(grid, GRIDSIZE, score);
-      if(movedLeft) {
-        var haveEmptyCells = fillRandomCell(grid, GRIDSIZE);
-        if(!haveEmptyCells) {
-          console.log(config.overMsg);
-          process.exit();
-        }
-      }
-      displayGrid(grid, score);
-      break;
+        case "left":
+        case "1":
+            let movedLeft = moveGridToLeft(grid, GRIDSIZE, score);
+            if (movedLeft) {
+                var haveEmptyCells = fillRandomCell(grid, GRIDSIZE);
+                if (!haveEmptyCells) {
+                    console.log(config.overMsg);
+                    process.exit();
+                }
+            }
+            displayGrid(grid, score);
+            break;
 
-    default:
-      console.log('Please use valid keys.')
-      break;
-  }
+        default:
+            console.log('Please use valid keys.')
+            break;
+    }
 
-  /**
-   * Exit on pressing 'q' key
-  */
-  if (key && key.name == "q") {
-    process.exit();
-  }
+    /**
+     * Exit on pressing 'q' key
+     */
+    if (key && key.name == "q") {
+        process.exit();
+    }
 });
